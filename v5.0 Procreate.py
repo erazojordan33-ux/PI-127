23f86445d0ceff8f162b8e9b061abb01d8ef6ace
erazojordan33-ux <erazojordan33@gmail.com>
2025-10-29 13:11:31 -0500

    v5.0 Procreate.py

--- v5.0 Procreate.py
+++ v5.0 Procreate.py
@@ -701,38 +701,21 @@ if archivo_excel:
                 st.subheader("📋 Tareas con Fechas Calculadas y Ruta Crítica")
                 cols1 = [
                     'IDRUBRO','RUBRO','PREDECESORAS','FECHAINICIO','FECHAFIN',
-                    'DURACION_EFECTIVA','RENDIMIENTO','RUTA_CRITICA'
+                    'DURACION_EFECTIVA','UNIDAD_RUBRO','RENDIMIENTO','RUTA_CRITICA'
                 ]
 
                 cols = [
                     'IDRUBRO','RUBRO','PREDECESORAS','FECHAINICIO','FECHAFIN',
                     'FECHA_INICIO_TEMPRANA','FECHA_FIN_TEMPRANA',
                     'FECHA_INICIO_TARDE','FECHA_FIN_TARDE',
-                    'DURACION_EFECTIVA','HOLGURA_TOTAL','RUTA_CRITICA'
+                    'DURACION_EFECTIVA','UNIDAD_RUBRO','RENDIMIENTO','HOLGURA_TOTAL','RUTA_CRITICA'
                 ]
 
                 columnas_editables = ['PREDECESORAS', 'FECHAINICIO', 'FECHAFIN', 'RUTA_CRITICA']
                 column_config = {col: {"editable": (col in columnas_editables)} for col in cols1}
-
-                column_config["RENDIMIENTO"] = st.column_config.NumberColumn(
-                    "Rendimiento",
-                    help="Rendimiento efectivo por día de trabajo",
-                    format="%.4f",  # 4 decimales
-                )
-
-                tareas_mostrar = st.session_state.tareas_df.copy()
-                
-                tareas_mostrar["RENDIMIENTO_FORMAT"] = tareas_mostrar.apply(
-                    lambda x: f"{x['RENDIMIENTO']:.4f} {x['UNIDAD_RUBRO']}/día"
-                    if pd.notna(x["RENDIMIENTO"]) and pd.notna(x["UNIDAD_RUBRO"])
-                    else "",
-                    axis=1
-                )
-                
-                cols1_mostrar = [c if c != "RENDIMIENTO" else "RENDIMIENTO_FORMAT" for c in cols1]
                 
                 tareas_editadas = st.data_editor(
-                    tareas_mostrar[cols1_mostrar],
+                    tareas_mostrar[cols1],
                     key="tareas_editor",
                     use_container_width=True,
                     column_config=column_config
@@ -1279,6 +1262,7 @@ if archivo_excel:
 
 else:
     st.warning("Sube el archivo Excel con las hojas Tareas, Recursos y Dependencias.")
+
 
 
 
